e20ce5bed40dbb4e95313d00d77133032ff6c6ed
BinduVinod <bindoos.vinod@gmail.com>
2019-12-10 22:42:38 +0000

    Added simple chrome browser invocation

--- src/com/learn/selenium/BrowserInvocation.java
+++ src/com/learn/selenium/BrowserInvocation.java
@@ -1,10 +1,19 @@
 package com.learn.selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BrowserInvocation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\FILE\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
+			
 	}
 
 }
